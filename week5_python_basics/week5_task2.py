55610ece5c1837a23d72717f200a1b6336bb9048
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-09 01:12:37 +0100

    Stretch and challenge for task 2 completed.

--- week5_python_basics/week5_task2.py
+++ week5_python_basics/week5_task2.py
@@ -2,6 +2,7 @@
 This module is for task 2: calculations. I will practice using python
 to do math.
 '''
+import math
 
 #floats will be used for the data type to ensure there is no error if the user
 #enters a float as opposed to an integer.
@@ -35,3 +36,13 @@ print(floor_division)
 
 power_of = pow(number_1, number_3)
 print(power_of)
+
+#stretch and challenge
+#I have seen different formulas for surface area so this may not be correct
+radius = float(input('What is the radius?'))
+depth = float(input('What is the depth?'))
+total_volume = math.pi * radius * radius * depth
+surface_area = ((2*math.pi*radius) * depth) + ((math.pi*radius**2)*2)
+
+print('The total volume of is: ', round(total_volume, 3))
+print('The surface area is: ', round(surface_area, 3))
