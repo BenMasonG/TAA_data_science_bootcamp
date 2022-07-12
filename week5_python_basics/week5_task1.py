6f87632a518ec42184c39aae11f0927c4f666413
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-12 02:41:32 +0100

    task 1 amended to add a favourite meal function that will print a statement saying what a persons favourite meal is

--- week5_python_basics/week5_task1.py
+++ week5_python_basics/week5_task1.py
@@ -3,9 +3,7 @@ The purpose of this module is to idenfiy the users favourite meal
 and print a statment stating what their favourite courses are.
 '''
 
-from decimal import Rounded
-
-
+name = input('What is your name?').title()
 starter = input('What is your favourite starter?').lower()
 main = input('What\'s your favourite main course?').lower()
 dessert = input('What\'s your favourite dessert?').lower()
@@ -21,4 +19,23 @@ bill = float(input('How much is the bill please?'))
 number_of_diners = int(input('How many people are dinning?'))
 cost_per_person = round(bill/number_of_diners, 2)
 
-print(cost_per_person)
+print('The bill will be £' + str(cost_per_person), 'per person')
+
+def favourite_meal(name, starter, main, dessert, drink):
+    '''
+    Tell people what a person's favourite meal is.4
+
+    :param name: string - the person's name.
+    :param starter: string - their favourite starter.
+    :param main: string - their favourite main course.
+    :param dessert: string - their favourite dessert.
+    :param drink: main - their favourite drink.
+
+    return: string - a sentence stating the person's favourite meal.
+    '''
+
+    print ( name + '\'s', 'favourite meal is ' + starter + ' for their starter '
+      + main + ' for their main course and', dessert +
+      ' for their desesert with a glass of', drink + ' to wash it down.')
+
+favourite_meal(name, starter, main, dessert, drink)
