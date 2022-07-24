42efb4f4a5073b98afa7ab4f4ffda0b6778cfddb
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-25 00:38:24 +0100

    Fruity Loops task amended to raise a TypeError if the argument passed is not a string.

--- week07_python_fundamentals/Task2_fruity_loops.py
+++ week07_python_fundamentals/Task2_fruity_loops.py
@@ -1,4 +1,12 @@
 def vowel_counter(fruit):
+    '''
+    This funtion takes a string and returns the number of values in the string.
+    :param fruit: str - the name of a fruit.
+    :return: The number of vowels in that fruit.
+    '''
+
+    if not isinstance(fruit, str):
+        raise TypeError('This function only accept strings as an argument')
     count_vowels = 0
     for letter in fruit:
         if letter.lower() in 'aeiou':
@@ -8,4 +16,10 @@ def vowel_counter(fruit):
     if count_vowels > 1:
         return f'There are {count_vowels} vowels in the word {fruit}'
     if count_vowels == 0:
-        return f'Wow, you found a fruit that has 0 vowels in its name!'
+        return 'Wow, you found a fruit that has no vowels in its name!'
+
+vowel_counter('Banana')
+vowel_counter('Orange')
+vowel_counter('Pear')
+vowel_counter('zzzz')
+vowel_counter(123)
