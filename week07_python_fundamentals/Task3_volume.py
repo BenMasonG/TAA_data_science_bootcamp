9bd8beee05194fa19bbdf3204029cb4e91e68e89
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-25 01:30:39 +0100

    Minor amendments made to tasks 3 and 4.

--- week07_python_fundamentals/Task3_volume.py
+++ week07_python_fundamentals/Task3_volume.py
@@ -1,13 +1,15 @@
+'''
+For task 3 of the week 7 Home Learning Tasks. The functions calculates the
+volume of a box. The second function does the same but rounds the answer to
+2 decimal places.
+'''
 def box_volume(width, depth, height):
-    volume = width * depth *height
+    volume = float(width * depth *height)
     return volume
 
 def box_volume_rounded(width, depth, height):
-    volume = width * depth *height
+    volume = float(width * depth *height)
     return round(volume,2)
 
-#There is need to specify the input type. If a float is entered as a paramater the output will
-#be a float. It will be an int if all paramaters are ints.
-
 box_volume(10.12, 14.5, 41)
-box_volume_rounded(11,5,6)
+box_volume_rounded(11.3,5.23,6)
--- week07_python_fundamentals/Task4_pythagoras.py
+++ week07_python_fundamentals/Task4_pythagoras.py
@@ -6,7 +6,7 @@ def pythagoras_theorem(side_a, side_b, side_c):
     angle triangle and the third is '?'. The '?' can be any paramater and
     represents an unknown length. The function returns the length of the 
     unknown paramater.
-    
+
     :param side_a: int, float or string -This is one of the shorter sides of
     the triangle. If the value is known the input should be an int or flaot.
     If the value is unknown it should be '?'.
@@ -23,7 +23,7 @@ def pythagoras_theorem(side_a, side_b, side_c):
     '''
     if (side_a == '?' and side_b == '?') or (side_a == '?' and side_c == '?')\
         or (side_b == '?' and side_c == '?'):
-            raise ValueError('Only one input can be unknown.')
+        raise ValueError('Only one input can be unknown.')
     if side_a == '?':
         unknown_side = sqrt((side_c * side_c) - (side_b * side_b))
         return f'Side a equals {unknown_side}.'
@@ -37,3 +37,6 @@ def pythagoras_theorem(side_a, side_b, side_c):
 pythagoras_theorem('?', 3, 6)
 pythagoras_theorem(3, '?', 6)
 pythagoras_theorem(3, 3, '?')
+
+#I have not asked the user for input as the function takes the required information as
+#arguments.
