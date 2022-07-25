6b163d442bbae9122d10d7a802f7aeba4f64d669
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-25 01:03:14 +0100

    task 2 has been amended. A second function has been created to loop through a list of strings.

--- week07_python_fundamentals/Task2_fruity_loops.py
+++ week07_python_fundamentals/Task2_fruity_loops.py
@@ -1,3 +1,6 @@
+'''
+This module is for task 2 of the week 7 Home Learning Tasks. 
+'''
 def vowel_counter(fruit):
     '''
     This funtion takes a string and returns the number of values in the string.
@@ -22,4 +25,22 @@ vowel_counter('Banana')
 vowel_counter('Orange')
 vowel_counter('Pear')
 vowel_counter('zzzz')
-vowel_counter(123)
+
+
+def list_vowel_counter(fruit_list):
+    '''
+    A function to iterate through a list of fruits and return the number of vowels
+    in each fruit.
+
+    :param fruit_list: list of str - a list of strings.
+    :return: A list stating how many vowels were in each string in the inital list
+    '''
+
+    vowels_per_fruit = []
+    for fruit in fruit_list:
+        vowels_per_fruit.append(vowel_counter(fruit))
+    return vowels_per_fruit
+
+list_of_fruits = ['apple', 'banana', 'pear']
+
+list_vowel_counter(list_of_fruits)
